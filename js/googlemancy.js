d8447915cea798f59773e2de126e0018691bf67c
Lin Zhang <lin.zhang.ux@gmail.com>
2018-05-03 02:44:34 -0400

    add partsOfsentences to pg. There is a unsolved bug.

--- js/googlemancy.js
+++ js/googlemancy.js
@@ -1,14 +1,109 @@
 // on page load, search for & display a random gif matching your search term using the Giphy API.
 document.addEventListener('DOMContentLoaded', init());
+/*
+* This function generates a poem from pre-defined sentence structure
+* and list of words
+*
+*/
 
 function init(){
   // loadJSON(function(data){
   //     console.log(data);
   // });
+  var pg = {
+    numSentences: 6,
+    numSentencePatterns: null,
+    // hard-coded sentence patterns is the simpler way
+    sentencePatterns: [
+      ['article', 'adjective', 'noun', 'verb', 'adverb', 'preposition', 'article', 'noun'],
+      ['pronoun', 'adverb', 'verb', 'noun', 'preposition', 'article', 'noun']
+    ],
+    languageParts: {
+      'vowel': 'aeiou'.split(''),
+      'article': {
+        'beforeVowel': 'the|an'.split('|'),
+        'beforeConsonant': 'the|a'.split('|')
+      },
+      'preposition': 'to|through|under|over|between|on|in|above|below|betwixt'.split('|'),
+      // 'adjective': 'beautiful|tall|flowing|hot|cold|fragrant|misty|bare|coarse|blind|dim|dreary|elaborate|enchanting|gleaming|glistening|green|organic|tender|cloudless'.split('|'),
+      'adverb': 'quickly|slowly|boldly|always|angrily|cheerfully|elegantly|frantically|innocently|nervously|powerfully|rarely|silently|wildly|warmly|solemly'.split('|'),
+      // 'noun': 'hair|finger|sun|field|arm|sphere|rock|sand|grass|tree|flower|orb|sea|water|ocean|tide|sky|cloud|moon|star|cosmos|ant|otter|elephant'.split('|'),
+      'pronoun': 'he|she|it|someone'.split('|'),
+      // 'verb': 'kicks|moves|swings|runs|walks|flies|sprays|stings|drops|breaks|explodes|diminishes|sweetens|falls|rises|hears|floats'.split('|'),
+      'stop': '.|?|!'.split('|'),
+      'pause': ',|;|...| - |'.split('|')
+    },
+    setLanguageParts: function(partsOfSpeech){
+      var temp = {};
+      for(var key in this.languageParts) temp[key] = this.languageParts[key];
+      for(var key in partsOfSpeech) temp[key] = partsOfSpeech[key];
+      this.languageParts = temp;
+      console.log("languageParts: "+this.languageParts);
+    },
+    init: function() {
+      this.numSentencePatterns = this.sentencePatterns.length;
+    },
+    generateSentences: function(numSentences, markupBefore, markupAfter) {
+      var numSentences = numSentences || this.numSentences;
+      var markupBefore = markupBefore || '';
+      var markupAfter = markupAfter || '';
+      var sentences = [];
+
+      while (numSentences--) {
+        var sentence = '';
+        var sentencePattern = this.sentencePatterns[ randomInt(0, this.numSentencePatterns - 1) ];
+
+        // loop through sentence pattern array
+        for (var i = 0, length = sentencePattern.length; i < length; i++) {
+          var languagePartArray;
+          var articleType;
+          var nextWord = null;
+
+          // if this word is an article, need to determine if next word starts with a vowel or consonant
+          if (sentencePattern[i] === 'article') {
+            // get next word
+            var nextWordLanguagePartArray = this.languageParts[ sentencePattern[i + 1] ];
+            var nextWord = nextWordLanguagePartArray[ randomInt(0, nextWordLanguagePartArray.length - 1) ];
+
+            // set article type based on whether next word starts with vowel or consonant
+            if (this.languageParts['vowel'].indexOf(nextWord[0]) !== -1) {
+              articleType = 'beforeVowel';
+            } else {
+              articleType = 'beforeConsonant';
+            }
+
+            languagePartArray = this.languageParts[ sentencePattern[i] ][ articleType ];
+          } else {
+            languagePartArray = this.languageParts[ sentencePattern[i] ];
+          }
+
+          // add this word to sentence
+          sentence += languagePartArray[ randomInt(0, languagePartArray.length - 1) ] + ' ';
+
+          // if next word was gotten, also add next word to sentence and increment the i counter
+          if (nextWord !== null) {
+            sentence += nextWord + ' ';
+            i++;
+          }
+        }
+        sentences.push(markupBefore + sentence.trim() + markupAfter);
+        console.log(sentence);
+
+      } // end while (numSentences--)
+
+      return sentences;
+
+    } // end generateSentences()
+  } // end poetryGenerator
+
   var htmltext = "The picture is clear before me of the day I first walked. My mother, a handsome, cold-eyed woman, who did not love me, had driven out from town to nurse's cottage. I shut my eyes, and I am back in the little parlour with its spindle chairs, an old-fashioned piano with green silk front, its pink-flowered wall-paper, and the two wonderful black-and-white dogs on the mantelpiece. There were two pictures I loved to gaze upon—Robert Emmett in the dock, and Mary Stuart saying farewell to France. I do not remember my mother's coming or going. Memory begins to work from the moment nurse put me on a pair of unsteady legs. There were chairs placed for me to clutch, and I was coaxingly bidden to toddle along, over to mamma. It was very exciting. First one chair had to be reached, then another fallen over, till a third tumbled me at my mother's feet. I burst into a passion of tears, not because of the fall, but from terror at finding myself so near my mother. Nurse gathered me into her arms and began to coo over me, and here the picture fades from my mind.My nurse loved me devotedly, and of course spoiled me. Most of the villagers helped her in this good work, so that the first seven years of my childhood, in spite of baby-face unblest by mother's kiss, were its happiest period. Women who do not love their children do well to put them out to nurse. The contrast of my life at home and the years spent with these rustic strangers is very shocking. The one petted, cherished, and untroubled; the other full of dark terrors and hate, and a loneliness such as grown humanity cannot understand without experience of that bitterest of all tragedies—unloved and ill-treated childhood. But I was only reminded of my sorrow at nurse's on the rare occasion of my mother's visits, or when nurse once a month put me into my best clothes, after washing my face with blue mottled soap—a thing I detested—and carried me off on the mail-car totown to report my health and growth. This was a terrible hour for me. From a queen I fell to the position of an outcast. My stepfather alone inspired me with confidence. He was a big handsome man with a pleasant voice, and he was always kind to me in a genial, thoughtless way. He would give me presents which my mother would angrily seize from me and give to her other children, not from love, for she was hardly kinder to them than to me, but from an implacable passion to wound, to strike the smile from the little faces around her, to silence a child's laughter with terror of herself. She was a curious woman, my mother. Children seemed to inspire her with a vindictive animosity, with a fury for beating and banging them, against walls, against chairs, upon the ground, in a way that seems miraculous to me now how they were saved from the grave and she from the dock. She had a troop of pretty engaging children, mostly girls, only one of whom she was ever known to kiss or caress, and to the others she was worse than the traditional stepmother of fairy tale. It was only afterwards I learned that those proud creatures I, in my abject solitude, hated and envied, lived in the same deadly fear[Pg 4] of her with which her cold blue eyes and thin cruel lips inspired me with.But there were, thank God! many bright hours for me, untroubled by her shadow. I was a little sovereign lady in my nurse's kindly village, admired and never thwarted. I toddled imperiously among a small world in corduroy breeches and linsey skirts, roaming unwatched the fields and lanes from daylight until dark. We sat upon green banks and made daisy chains, and dabbled delightedly with the sand of the pond edges, while we gurgled and chattered and screamed at the swans. The setting of that nursery biography is vague. It seemed to me that the earth was made up of field beyond field, and lanes that ran from this world to the next, with daisies that never could be gathered, they were so many; and an ocean since has impressed me less with the notion of immensity of liquid surface than the modest sheet of water we called the Pond. Years afterwards I walked out from town to that village, and how small the pond was, how short the lanes, what little patches for fields so sparsely sprinkled with daisies! A more miserable disillusionment I have not known. I have always marvelled at the roll of [Pg 5]reminiscences and experiences of childhood told consecutively and with coherence. Children live more in pictures, in broken effects, in unaccountable impulses that lend an unmeasured significance to odd trifles to the exclusion of momentous facts, than in story. This alone prevents the harmonious fluency of biography in an honest account of our childhood. Memory is a random vagabond, and plays queer tricks with proportion. It dwells on pictures of relative unimportance, and revives incidents of no practical value in the shaping of our lives. Its industry is that of the idler's, wasteful, undocumentary, and untrained. For vividness without detail, its effects may be compared with a canvas upon which a hasty dauber paints a background of every obscure tint in an inextricable confusion, and relieves it with sharply defined strokes of bright colour. Jim Cochrane, my everyday papa, as I called him, was a sallow-faced man with bright black eyes, which he winked at me over the brim of his porter-measure, as he refreshed himself at the kitchen fire after a hard day's work. He was an engine-driver, and once took me on the engine with him to the nearest station, he and a comrade holding me tight between them, while[Pg 6] I shrieked and chattered in all the bliss of a first adventure.This is a memory of sensation, not of sight. I recall the rush through the air, the sting, like needle-points against my cheeks and eyelids, of the bits of coal that flew downward from the roll of smoke, the shouting men laughing and telling me not to be afraid, the red glare of the furnace whenever they slid back the grate opening, the whiff of fright and delight that thrilled me, and, above all, the confidence I had that I was safe with nurse's kind husband.Poor Jim! His was the second dead face I looked upon without understanding death. The ruthless disease of the Irish peasant was consuming him then, and he died before he had lived half his life through.";
-  htmltext.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-  console.log(htmltext);
-  generateAnswer(htmltext);
+  htmltext = htmltext.replace(/[^0-9a-zA-Z\s]/gi, '');
+  console.log("Print out replaced txt" + htmltext);
+  var partsOfSpeech = parseSentencs(htmltext);
+  pg.init();
+  pg.setLanguageParts(partsOfSpeech);
+  var poem = pg.generateSentences(8, "<p>", "</p>");
+  console.log("Let's read some fancy ass poem: "+ poem);
 }
 
 window.onload = function(){
@@ -146,11 +241,39 @@ function getSearchResults(q){
 /*
 * This function parse the html file into sentence parts using compromise.js
 */
-
-function generateAnswer(t){
+function parseSentencs(t){
   let doc = nlp(t);
-  var nouns = doc.sentences().match('#Noun').out('text');
-  console.log(nouns);
+  let pos = {};
+  var nouns = doc.sentences().match('#Noun').out('text').replace(/\s+/g, ',').split(',');
+  var verbs = doc.sentences().match('#Verb').out('text').replace(/\s+/g, ',').split(',');
+  var adjectives = doc.sentences().match('#Adjective').out('text').replace(/\s+/g, ',').split(',');
+
+  pos = {
+    'nouns': nouns,
+    'verbs': verbs,
+    'adjectives': adjectives
+  }
+  console.log("print out nouns: " + pos);
+  return pos;
 }
+// -- UTILITY FUNCTIONS
+function randomInt(min, max) {
+return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+String.prototype.trim == String.prototype.trim || function() {
+return this.replace(/^\s+|\s+$/g, '');
+}
+
+// ----------------------------------------------------------------------------
+
+  // initialize poetry generator
+  // pg.init();
+  //
+  // $('.generate').on('click', function() {
+  //   var sentences = pg.generateSentences( $('#num-sentences').val(), '<p>', '</p>' );
+  //   $('#poetry-content').html( sentences.join('') );
+  // })
+
 // append the query parameter key=yourAPIKey to all request URLs. The API key is safe for embedding in URLs, it doesn't need any encoding.
 //AIzaSyCeIkbqWdO1sqWJXDF361F9Xx4iI3Kwovc
